c23a97b5768a2c7ccbd0334d37a47e7ea3860434
moadhamousti <moadhamousti@gmail.com>
2024-12-22 17:52:38 +0100

    Hero Animation Update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,47 +1,135 @@
+"use client";
+
 import Button from "@/components/Button";
-import DesgignForm1Image from "@/assets/images/design-example-1.png"
-import DesgignForm2Image from "@/assets/images/design-example-2.png"
+import DesgignForm1Image from "@/assets/images/design-example-1.png";
+import DesgignForm2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
-
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
+import cursorYouImage from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+    const [leftDesignScope, leftDesignAnimate] = useAnimate();
+    const [leftPointerScope, leftPointerAnimate] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+    useEffect(() => {
+        // Left Design
+        leftDesignAnimate([
+            [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+        leftPointerAnimate([
+            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                { y: [0, 16, 0], x: 0 },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+
+        // Right Design
+        rightDesignAnimate([
+            [
+                rightDesignScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+        rightPointerAnimate([
+            [
+                rightPointerScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightPointerScope.current, { y: 0, x: 175 }, { duration: 0.5 }],
+            [
+                rightPointerScope.current,
+                { y: [0, 20, 0], x: 0 },
+                { duration: 0.5 },
+            ],
+        ]);
+    }, []);
+
     return (
-    <section className="py-24 overflow-x-clip">
-        <div className="container relative">
-            <div className="absolute -left-32 top-16 hidden lg:block">
-                <Image src={DesgignForm1Image} alt="Desgign Form 1 Image"/>
-            </div>
-            <div className="absolute -right-64 -top-16 hidden lg:block">
-                <Image src={DesgignForm2Image} alt="Desgign Form 2 Image"/>
-            </div>
-            <div className="absolute left-56 top-96 hidden lg:block">
-                <Pointer name="Andrea"/>
-            </div>
-            <div className="absolute right-80 -top-4 hidden lg:block">
-                <Pointer name="Bryan" color="red"/>
-            </div>
-            <div className="flex justify-center">
-                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
-                ✨ $7.5M seed round raised
-                </div>
-            </div>
-            <h1 className="md:text-7xl text-6xl font-medium text-center mt-6 lg:text-8xl">Impactful design, created effortlessly </h1>
-            <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Design tools shouldn&apos;t slow you down. Layers combines powerful 
-                features with intuitive interface that keeps you in your creative flow.
-            </p>
-            <form className="flex border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
-                <input type="email" className="bg-transparent px-4 md:flex-1 w-full" placeholder="Enter your email"/>
-                <Button 
-                    type="submit" 
-                    variant="primary"
-                    className="whitespace-nowrap"
-                    size="sm"
+        <section
+            className="py-24 overflow-x-clip"
+            style={{
+                cursor: `url(${cursorYouImage.src}), auto`,
+            }}
+        >
+            <div className="container relative">
+                <motion.div
+                    ref={leftDesignScope}
+                    initial={{ opacity: 0, y: 100, x: -100 }}
+                    drag
+                    className="absolute -left-32 top-16 hidden lg:block"
                 >
-                    Sign Up
-                </Button>
-            </form>
-        </div>
-    </section>
+                    <Image
+                        src={DesgignForm1Image}
+                        draggable="false"
+                        alt="Desgign Form 1 Image"
+                    />
+                </motion.div>
+                <motion.div
+                    ref={leftPointerScope}
+                    initial={{ opacity: 0, y: 100, x: -200 }}
+                    className="absolute left-56 top-96 hidden lg:block"
+                >
+                    <Pointer name="Andrea" />
+                </motion.div>
+                <motion.div
+                    ref={rightDesignScope}
+                    initial={{ opacity: 0, y: 100, x: 100 }}
+                    drag
+                    className="absolute -right-64 -top-16 hidden lg:block"
+                >
+                    <Image
+                        src={DesgignForm2Image}
+                        draggable="false"
+                        alt="Desgign Form 2 Image"
+                    />
+                </motion.div>
+                <motion.div
+                    ref={rightPointerScope}
+                    initial={{ opacity: 0, y: 100, x: 275 }}
+                    className="absolute right-80 -top-4 hidden lg:block"
+                >
+                    <Pointer name="Bryan" color="red" />
+                </motion.div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ $7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="md:text-7xl text-6xl font-medium text-center mt-6 lg:text-8xl">
+                    Impactful design, created effortlessly{" "}
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with intuitive interface that keeps you in
+                    your creative flow.
+                </p>
+                <form className="flex border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input
+                        type="email"
+                        className="bg-transparent px-4 md:flex-1 w-full"
+                        placeholder="Enter your email"
+                    />
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="whitespace-nowrap"
+                        size="sm"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
     );
 }
